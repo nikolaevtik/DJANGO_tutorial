5dc1369d8857266225cf75c7b5dbbb09c9edf68a
Maksim Nikolaev <231406573+nikolaevtik@users.noreply.github.com>
2026-05-29 15:21:19 +0300

    work with views

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,3 +1,6 @@
+from django_stubs_ext import monkeypatch
+monkeypatch()
+
 import datetime
 
 
@@ -18,6 +21,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+   
+    
     
     def __str__(self):
         return self.choice_text
